652502b3aed296228260b800d92ea5c4e43fe430
Marcos Ribeiro <ortolane@gmail.com>
2010-12-07 10:51:47 -0200

    All you base are belong to us.

---
+++ core/views.py
@@ -0,0 +1,10 @@
+# Create your views here.
+
+from django.http import HttpResponse
+from django.template import loader, Context
+
+def homepage(request):
+    t = loader.get_template('index.html')
+    c = Context()
+    content = t.render(c)
+    return HttpResponse(content)
